7102f5fc83c5e9c902dfd368770a7f9d2dcf2640
pleshw <pleshw@outlook.com>
2024-05-04 07:53:38 -0300

    better loading screen

--- scripts/GameHints/HintMessages.cs
+++ scripts/GameHints/HintMessages.cs
@@ -8,7 +8,7 @@ public static class HintMessages
   {
     public static readonly RandomList<Hint> Hints = [
       new Hint {Message="Mate monstros para adquirir experiência e aprimorar suas habilidades.", TimeInMs = 3000},
-      new Hint {Message="Alguns lugares possuem uma aura liminar. Dizem que esses lugares podem te levar para outras dimensões.", TimeInMs = 4000},
+      new Hint {Message="Alguns lugares possuem uma aura liminar. Dizem que esses lugares podem te levar para outras dimensões.", TimeInMs = 5000},
       new Hint {Message="Você pode fazer amizade com outras criaturas. Tente entregar algo que elas gostem.", TimeInMs = 4000},
     ];
   }
